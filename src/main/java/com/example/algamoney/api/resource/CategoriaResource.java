7142a8d90afcdb1e7d0ac57e5828f36167556626
rospiel <rospielberg@gmail.com>
2017-12-09 16:48:49 -0200

    Implementando [POST] criação de categoria e [GET] por código categoria

--- src/main/java/com/example/algamoney/api/resource/CategoriaResource.java
+++ src/main/java/com/example/algamoney/api/resource/CategoriaResource.java
@@ -1,11 +1,21 @@
 package com.example.algamoney.api.resource;
 
+import java.net.URI;
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.algamoney.api.model.Categoria;
 import com.example.algamoney.api.repository.CategoriaRepository;
@@ -38,5 +48,42 @@ public class CategoriaResource {
 		/* return categorias.isEmpty() != true ? ResponseEntity.ok(categorias) : ResponseEntity.notFound().build(); 404 recurso não identificado */
 		/* return categorias.isEmpty() != true ? ResponseEntity.ok(categorias) : ResponseEntity.noContent().build(); 204 busquei mas não tenho o que te mostrar */
 		return categorias;
+	}
+	
+	/**
+	 * 
+	 * @param categoria
+	 * @param response
+	 * 
+	 * Método que incluí no banco uma nova categoria e devolve na reposta endereço do recurso e o objeto em questão
+	 * @RequestBody --> Converte automaticamente pro objeto Categoria
+	 * @ResponseStatus --> Indica o código http de retorno que enviaremos
+	 */
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria, HttpServletResponse response) {
+		Categoria categoriaSalva = categoriaRepository.save(categoria);
+		
+		/* Por meio da requisição atual construa a localização do recurso recém criado */
+		URI endereco = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}").buildAndExpand(categoriaSalva.getCodigo()).toUri();
+		
+		/* Cadastrando na resposta a localização do recurso */
+		response.setHeader("Location", endereco.toASCIIString());
+		
+		/* Devolvendo o json do recurso recém criado */
+		return ResponseEntity.created(endereco).body(categoriaSalva);
+	}
+	
+	/**
+	 * 
+	 * @param codigo
+	 * @return
+	 *  
+	 * Método que busca um recurso pelo código
+	 * @PathVariable --> Indica que o parâmetro código receberá o atributo de mesmo nome passado no endereço
+	 */
+	@GetMapping("/{codigo}")
+	public Categoria buscarPeloCodigo(@PathVariable Long codigo) {
+		return categoriaRepository.findOne(codigo);
 	}
 }
