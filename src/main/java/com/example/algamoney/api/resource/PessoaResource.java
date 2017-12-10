100c2328ddf1f53bbce84d6072a70e5f1fb4dfc2
rospiel <rospielberg@gmail.com>
2017-12-10 14:45:03 -0200

    Implementação da remoção de recurso no controller de pessoa.

--- src/main/java/com/example/algamoney/api/resource/PessoaResource.java
+++ src/main/java/com/example/algamoney/api/resource/PessoaResource.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -78,5 +79,17 @@ public class PessoaResource {
 		publicarEvento.publishEvent(new RecursoCriadoEvent(this, response, pessoaSalva.getCodigo()));
 		
 		return ResponseEntity.status(HttpStatus.CREATED).body(pessoaSalva);
+	}
+	
+	/**
+	 * 
+	 * @param codigo
+	 * 
+	 * Método que remove um recurso e retorna o código 204 
+	 */
+	@DeleteMapping("/{codigo}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void excluir(@PathVariable Long codigo) {
+		pessoaRepository.delete(codigo);
 	}
 }
