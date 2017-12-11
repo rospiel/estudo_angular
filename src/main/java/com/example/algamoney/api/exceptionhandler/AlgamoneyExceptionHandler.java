0b8784701beeec12ed228a3ac396ef59fabe83c1
rospiel <rospielberg@gmail.com>
2017-12-11 20:29:52 -0200

    A cargo do AlgamoneyExceptionHandler validar inclusão lançamento Inclusão de biblioteca pra captura de erro transparente

--- src/main/java/com/example/algamoney/api/exceptionhandler/AlgamoneyExceptionHandler.java
+++ src/main/java/com/example/algamoney/api/exceptionhandler/AlgamoneyExceptionHandler.java
@@ -4,9 +4,11 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -70,6 +72,21 @@ public class AlgamoneyExceptionHandler extends ResponseEntityExceptionHandler {
 		String mensagemDesenvolvedor = erro.toString();
 		List<Erro> erros = Arrays.asList(new Erro(mensagem, mensagemDesenvolvedor));
 		return handleExceptionInternal(erro, erros, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+	}
+	
+	/**
+	 * Método que captura exceções do banco quanto a chave violada
+	 * ExceptionUtils --> Se faz necessário para transmissão de uma mensagem mais transparente ao desenvolvedor
+	 * @param erro
+	 * @param request
+	 * @return
+	 */
+	@ExceptionHandler({ DataIntegrityViolationException.class })
+	public ResponseEntity<Object> handleDataIntegrityViolationException(DataIntegrityViolationException erro, WebRequest request) {
+		String mensagem = mensagensCadastradas.getMessage("recurso.operacao-nao-permitida", null, LocaleContextHolder.getLocale());
+		String mensagemDesenvolvedor = ExceptionUtils.getRootCauseMessage(erro);
+		List<Erro> erros = Arrays.asList(new Erro(mensagem, mensagemDesenvolvedor));
+		return handleExceptionInternal(erro, erros, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
 	}
 	
 	/**
--- src/main/java/com/example/algamoney/api/service/LancamentoService.java
+++ src/main/java/com/example/algamoney/api/service/LancamentoService.java
@@ -4,16 +4,11 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.example.algamoney.api.event.RecursoCriadoEvent;
-import com.example.algamoney.api.model.Categoria;
 import com.example.algamoney.api.model.Lancamento;
-import com.example.algamoney.api.model.Pessoa;
-import com.example.algamoney.api.repository.CategoriaRepository;
 import com.example.algamoney.api.repository.LancamentoRepository;
-import com.example.algamoney.api.repository.PessoaRepository;
 
 /**
  * Classe responsável pelas tratativas de service do model Lancamento
@@ -22,12 +17,6 @@ import com.example.algamoney.api.repository.PessoaRepository;
  */
 @Service
 public class LancamentoService {
-	
-	@Autowired
-	private CategoriaRepository categoriaRepository;
-	
-	@Autowired
-	private PessoaRepository pessoaRepository;
 	
 	@Autowired
 	private LancamentoRepository lancamentoRepository;
@@ -41,25 +30,9 @@ public class LancamentoService {
 	 * @param response
 	 * @return
 	 * 
-	 * Tem a ação de incluir um lançamento, porém antes verifica se a categoria e pessoa informadas existem
-	 * do contrário lança exceções quanto a não existência destas informações no sistema
+	 * Tem a ação de incluir um lançamento
 	 */
 	public Lancamento criar(Lancamento lancamento, HttpServletResponse response) {
-		StringBuilder mensagemErro = null;
-		Categoria categoriaBanco = categoriaRepository.findOne(lancamento.getCategoria().getCodigo());
-		
-		if(categoriaBanco == null) {
-			mensagemErro = new StringBuilder("Categoria de código " + lancamento.getCategoria().getCodigo() + " não encontrada.");
-			throw new EmptyResultDataAccessException(mensagemErro.toString(), 1);
-		}
-		
-		Pessoa pessoaBanco = pessoaRepository.findOne(lancamento.getPessoa().getCodigo());
-		
-		if(pessoaBanco == null) {
-			mensagemErro = new StringBuilder("Pessoa de código " + lancamento.getPessoa().getCodigo() + " não encontrada.");
-			throw new EmptyResultDataAccessException(mensagemErro.toString(), 1);
-		}
-		
 		Lancamento lancamentoSalvo = lancamentoRepository.save(lancamento);
 		
 		publicarEvento.publishEvent(new RecursoCriadoEvent(this, response, lancamentoSalvo.getCodigo()));
