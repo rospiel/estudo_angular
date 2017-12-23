7e353bf108a64f2df1fb3ebc5fb903e7f33ab333
rospiel <rospielberg@gmail.com>
2017-12-23 12:49:56 -0200

    Deixando mecanismo de autenticação dinâmico entre oauth e basic.

--- src/main/java/com/example/algamoney/api/config/AuthorizationServerConfig.java
+++ src/main/java/com/example/algamoney/api/config/AuthorizationServerConfig.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
@@ -18,13 +19,14 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 import com.example.algamoney.api.config.token.CustomTokenEnhancer;
 
-@Configuration
-@EnableAuthorizationServer
 /**
  * Classe de configuração para acesso aos recursos
  * @author Rodrigo
  *
  */
+@Profile("oauth-security")
+@Configuration
+@EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
 	
 	@Autowired
